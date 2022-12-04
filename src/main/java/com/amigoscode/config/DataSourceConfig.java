05f912292cbc89b17a4b6c75cf6a9d08d0a26ae6
Rony-dot <rakibulhassan.rony123@gmail.com>
2022-12-04 22:06:33 +0600

    added JdbcTemplate for HikariDataSource

--- src/main/java/com/amigoscode/config/DataSourceConfig.java
+++ src/main/java/com/amigoscode/config/DataSourceConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DataSourceConfig {
@@ -13,10 +14,15 @@ public class DataSourceConfig {
     @Bean
     @Primary
     @ConfigurationProperties("app.datasource.main")
-    public HikariDataSource hikariDataSource() {
+    public HikariDataSource hikariDataSourcePostgres() {
         return DataSourceBuilder
                 .create()
                 .type(HikariDataSource.class)
                 .build();
+    }
+
+    @Bean
+    public JdbcTemplate hikariJdbcTemplate(HikariDataSource hikariDataSource){
+        return new JdbcTemplate(hikariDataSource);
     }
 }
